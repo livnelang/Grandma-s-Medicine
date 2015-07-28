1f371f04eab90958fcff786054bd8d6a56cc4a51
livnelang <livnelang@gmail.com>
2015-07-28 16:12:47 +0300

    Update herbal.js

--- app/public/js/herbal.js
+++ app/public/js/herbal.js
@@ -10,7 +10,6 @@ var specific_herbal;
 	*/
 	var parameter = window.location.search.replace( "?", "" ); // will return the GET parameter 
 	var values = parameter.split("=");
-	console.log('incoming values: ' + values); // will return and array as ["name", "sample"] 
 	var query = values[0]; // Gives us or herbal or nothing
 	
 	grandmaApp.run(function ($http) {
@@ -20,9 +19,7 @@ var specific_herbal;
 	* Returns Herbals By Problem
 	*/
 	if(query == 'problem') {
-		console.log('inside problem angualar controller');
 		$http.get("https://grandmas-medicine.herokuapp.com/ws_grandma/getFilteredHerbal/" + values[1] ).success(function (data) {
-		console.log('entered browser request: ' + data);
 		model.items = data;
 			}).error(function (data, status, headers, config) {
 		        alert("error" + data + status);
@@ -36,7 +33,6 @@ var specific_herbal;
 	*/
 	if(query =='herbal') {
 		$http.get("https://grandmas-medicine.herokuapp.com/ws_grandma/getSpecificHerbal/" + values[1] ).success(function (data) {
-			console.log('entered browser request: ' + data);
 			model.specific_herbal = data;
 			//console.log('specific_herbal: ' +specific_herbal.title);
 			}).error(function (data, status, headers, config) {
@@ -50,9 +46,7 @@ var specific_herbal;
 		*/
 		if(query =='allherbals') { 
 				$http.get("https://grandmas-medicine.herokuapp.com/ws_grandma/getHerbalsData").success(function (data) {
-					console.log('entered browser request: ' + data);
 					model.items = data;
-					console.log("inside items: " + model.items[0].desc);
 				}).error(function (data, status, headers, config) {
 			        alert("error" + data + status);
 			        return status;
