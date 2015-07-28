677697fa16b421e93a2954c679cb18a335668fbc
livnelang <livnelang@gmail.com>
2015-07-28 16:14:06 +0300

    Update specific_herbal.js

--- app/public/js/specific_herbal.js
+++ app/public/js/specific_herbal.js
@@ -11,7 +11,6 @@ var model = {
 	*/
 	var parameter = window.location.search.replace( "?", "" ); // will return the GET parameter 
 	var values = parameter.split("=");
-	console.log('incoming values: ' + values); // will return and array as ["name", "sample"] 
 	var query = values[0]; // Gives us or herbal or nothing
 
 	
@@ -20,9 +19,7 @@ var model = {
 	* Returns Specific Herbal
 	*/
 		$http.get("https://grandmas-medicine.herokuapp.com/ws_grandma/getSpecificHerbal/" + values[1] ).success(function (data) {
-			console.log('entered browser request: ' + data);
 			model.specific_herbal = data;
-			console.log('qualities: ' +data.qualities);
 			}).error(function (data, status, headers, config) {
 			       alert("error" + data + status);
 			       return status;
